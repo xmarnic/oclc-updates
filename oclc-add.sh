afe2c285a72c0222e4a5619095539349bfa038dd
Nick Marlin <mail@nickmarl.in>
2024-08-02 19:21:47 +0000

    ♻️  rename `oclc-add.sh` to 'oclc-sftp.sh'

--- oclc-add.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# SFTP to OCLC
-sftp fx_wyzbl@filex-m1.oclc.org <<EOF
-cd /xfer/metacoll/in/bib
-put /software/WYLD/Unicorn/Xfer/1030040.WYZBL.wyld.1.mrc
-put /software/WYLD/Unicorn/Xfer/1040840.WYZBL.wyld.1.mrc
-exit
